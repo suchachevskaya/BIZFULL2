da6d03d5191280e29c69cf6a9b25f232786e02a4
suchachevskaya <116747974+suchachevskaya@users.noreply.github.com>
2024-03-12 02:11:00 -0700

    build

--- storage/framework/views/d1edab02fa9c227e04ef2e1256bcfe76.php
+++
@@ -1,6 +0,0 @@
-<div class="hidden sm:block">
-    <div class="py-8">
-        <div class="border-t border-gray-200"></div>
-    </div>
-</div>
-<?php /**PATH C:\OSPanel\BIZFULL2\resources\views/components/section-border.blade.php ENDPATH**/ ?>
